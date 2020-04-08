b0a30b9c4af377b13ceeca4cbda042b4cea8f7dc
planimir77 <34979738+planimir77@users.noreply.github.com>
2020-04-08 18:28:37 +0300

    Implement adding new user

---
+++ Web/CustomERP.Web.ViewModels/Administration/Register/UserInputViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace CustomERP.Web.ViewModels.Administration.Register
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class UserInputViewModel
+    {
+        [Required]
+        [MinLength(8)]
+        [MaxLength(50)]
+        [Display(Name = "Full Name")]
+        [RegularExpression(
+            "^([A-Z][a-z]+\\s[A-Z][a-z]+\\s[A-Z][a-z]+|[А-Я][а-я]+\\s[А-Я][а-я]+\\s[А-Я][а-я]+)")]
+        public string FullName { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string Position { get; set; }
+    }
+}
---
+++ Web/CustomERP.Web/Areas/Administration/Controllers/RegisterController.cs
@@ -0,0 +1,50 @@
+﻿namespace CustomERP.Web.Areas.Administration.Controllers
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using CustomERP.Data.Common.Repositories;
+    using CustomERP.Data.Models;
+    using CustomERP.Web.ViewModels.Administration.Register;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class RegisterController : AdministrationController
+    {
+        private readonly IDeletableEntityRepository<ApplicationUser> repository;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public RegisterController(IDeletableEntityRepository<ApplicationUser> repository, SignInManager<ApplicationUser> signInManager)
+        {
+            this.repository = repository;
+            this.signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            var viewModel = new UserInputViewModel();
+            return this.View(viewModel);
+        }
+
+        public async Task<IActionResult> CreateUser(UserInputViewModel viewModel)
+        {
+            var user = this.repository.All().FirstOrDefault(u => u.FullName == viewModel.FullName);
+            if (user == null)
+            {
+                user = new ApplicationUser
+                {
+                    FullName = viewModel.FullName,
+                    Position = viewModel.Position,
+                    CreatedFrom = this.User.Identities.FirstOrDefault()?.Name,
+                };
+                var y = this.User.Identities;
+
+                await this.repository.AddAsync(user);
+                await this.repository.SaveChangesAsync();
+                return this.Redirect("/Administration/Dashboard/Index");
+            }
+
+            return this.Content("Username already exist");
+        }
+    }
+}
