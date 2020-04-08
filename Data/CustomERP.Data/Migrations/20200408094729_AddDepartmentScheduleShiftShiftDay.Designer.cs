4a9754b79dfddda4624fde1d668febe2fea48f9f
planimir77 <34979738+planimir77@users.noreply.github.com>
2020-04-08 12:54:20 +0300

    Add new migration

--- Data/CustomERP.Data/Migrations/20200408094729_AddDepartmentScheduleShiftShiftDay.Designer.cs
+++ Data/CustomERP.Data/Migrations/20200408094729_AddDepartmentScheduleShiftShiftDay.Designer.cs
@@ -4,14 +4,16 @@ using CustomERP.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CustomERP.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200408094729_AddDepartmentScheduleShiftShiftDay")]
+    partial class AddDepartmentScheduleShiftShiftDay
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/CustomERP.Data/Migrations/20200408094729_AddDepartmentScheduleShiftShiftDay.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CustomERP.Data.Migrations
+{
+    public partial class AddDepartmentScheduleShiftShiftDay : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ModifiedFrom",
+                table: "Sections",
+                maxLength: 40,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeletedFrom",
+                table: "Sections",
+                maxLength: 40,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedFrom",
+                table: "Sections",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmentId",
+                table: "Sections",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ShiftId",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: true),
+                    CreatedFrom = table.Column<string>(nullable: true),
+                    ModifiedFrom = table.Column<string>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeletedOn = table.Column<DateTime>(nullable: true),
+                    DeletedFrom = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Schedules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: true),
+                    CreatedFrom = table.Column<string>(nullable: true),
+                    ModifiedFrom = table.Column<string>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeletedOn = table.Column<DateTime>(nullable: true),
+                    DeletedFrom = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: false),
+                    NumberOfDays = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedules", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShiftDays",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: true),
+                    CreatedFrom = table.Column<string>(nullable: true),
+                    ModifiedFrom = table.Column<string>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeletedOn = table.Column<DateTime>(nullable: true),
+                    DeletedFrom = table.Column<string>(nullable: true),
+                    WorkingMode = table.Column<int>(nullable: false),
+                    Begins = table.Column<DateTime>(nullable: false),
+                    Duration = table.Column<TimeSpan>(nullable: false),
+                    IncludingRest = table.Column<TimeSpan>(nullable: false),
+                    ScheduleId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShiftDays", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShiftDays_Schedules_ScheduleId",
+                        column: x => x.ScheduleId,
+                        principalTable: "Schedules",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shifts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: true),
+                    CreatedFrom = table.Column<string>(nullable: true),
+                    ModifiedFrom = table.Column<string>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeletedOn = table.Column<DateTime>(nullable: true),
+                    DeletedFrom = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: false),
+                    ScheduleId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shifts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Shifts_Schedules_ScheduleId",
+                        column: x => x.ScheduleId,
+                        principalTable: "Schedules",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sections_DepartmentId",
+                table: "Sections",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sections_Name",
+                table: "Sections",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_Name",
+                table: "Companies",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_FullName",
+                table: "AspNetUsers",
+                column: "FullName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_ShiftId",
+                table: "AspNetUsers",
+                column: "ShiftId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departments_IsDeleted",
+                table: "Departments",
+                column: "IsDeleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_IsDeleted",
+                table: "Schedules",
+                column: "IsDeleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShiftDays_IsDeleted",
+                table: "ShiftDays",
+                column: "IsDeleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShiftDays_ScheduleId",
+                table: "ShiftDays",
+                column: "ScheduleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shifts_IsDeleted",
+                table: "Shifts",
+                column: "IsDeleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shifts_ScheduleId",
+                table: "Shifts",
+                column: "ScheduleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Shifts_ShiftId",
+                table: "AspNetUsers",
+                column: "ShiftId",
+                principalTable: "Shifts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sections_Departments_DepartmentId",
+                table: "Sections",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Shifts_ShiftId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sections_Departments_DepartmentId",
+                table: "Sections");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "ShiftDays");
+
+            migrationBuilder.DropTable(
+                name: "Shifts");
+
+            migrationBuilder.DropTable(
+                name: "Schedules");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Sections_DepartmentId",
+                table: "Sections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Sections_Name",
+                table: "Sections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Companies_Name",
+                table: "Companies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_FullName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_ShiftId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentId",
+                table: "Sections");
+
+            migrationBuilder.DropColumn(
+                name: "ShiftId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ModifiedFrom",
+                table: "Sections",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 40,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DeletedFrom",
+                table: "Sections",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 40,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedFrom",
+                table: "Sections",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 40);
+        }
+    }
+}
