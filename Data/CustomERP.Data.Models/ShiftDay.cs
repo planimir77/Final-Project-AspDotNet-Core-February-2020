57bda1470892613d4a04b32091f91686972f1a57
planimir77 <34979738+planimir77@users.noreply.github.com>
2020-04-28 02:53:24 +0300

    Change Duration, Begins and IncludingRest Type Add Name Property Rename ShiftDay to ScheduleDay

--- Data/CustomERP.Data.Models/ShiftDay.cs
+++ Data/CustomERP.Data.Models/ShiftDay.cs
@@ -8,16 +8,18 @@
     public class ShiftDay : BaseDeletableModel<int>
     {
         [Required]
+        public int Name { get; set; }
+
+        [Required]
         public Mode WorkingMode { get; set; }
 
-        [Required]// TODO
-        public DateTime Begins { get; set; }
+        public string Begins { get; set; }
 
-        [Required]
-        public TimeSpan Duration { get; set; }
+        [Range(0, 12)]
+        public int? Duration { get; set; }
 
-        [Required]
-        public TimeSpan IncludingRest { get; set; }
+        [Range(0, 60)]
+        public int? IncludingRest { get; set; }
 
         [Required]
         public int ScheduleId { get; set; }
